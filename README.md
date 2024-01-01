0d041341916a6908d2ca2666029d0285203a3506
Sumit Kale <sumitkale788@gmail.com>
2024-01-01 21:48:07 +0530

    Code corrections

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# LeetCode Problem Solutions
+https://leetcode.com/problem-list/top-interview-questions/
 
-LeetCode Problem Solutions
+1. Solve easy difficulty questions first
+2. Think of solution not how you will write code
+3. Don't spend too much time on 1 question (max 45 min)
+4. Even if you have solved problem, check and understand top 3 solutions
--- mergeTwoSortedLists.js
+++ mergeTwoSortedLists.js
@@ -1,4 +1,6 @@
 /**
+ * https://leetcode.com/problems/merge-two-sorted-lists/
+ *
  * You are given the heads of two sorted linked lists list1 and list2.
  *
  * Merge the two lists into one sorted list.
@@ -13,22 +15,59 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function (list1, list2) {
-  let temp1 = list1;
-  let temp2 = list2;
-  let temp0;
+  let prev1 = list1;
+  let prev2 = list2;
+  let dummy = new ListNode(0);
+  let temp = dummy;
 
-  while (temp1 && temp2) {
-    temp0 = temp1;
-    if (temp1.val <= temp2.val) {
-      temp1 = temp1.next;
-      console.log("0:", temp0);
-      console.log("1:", temp1);
+  while (prev1 && prev2) {
+    if (prev1.val <= prev2.val) {
+      temp.next = prev1;
+      prev1 = prev1.next;
+      temp = temp.next;
     } else {
-      const newNode = new ListNode(temp2.val, temp0.next);
-      temp2 = temp2.next;
-      temp0.next = newNode;
+      temp.next = prev2;
+      prev2 = prev2.next;
+      temp = temp.next;
     }
   }
 
+  temp.next = prev1;
+  while (temp.next !== null) {
+    temp = temp.next;
+  }
+  temp.next = prev2;
+  list1 = dummy.next;
+
   return list1;
+};
+
+var mergeTwoLists2 = function (l1, l2) {
+  if (!l1) return l2;
+  else if (!l2) return l1;
+  else if (l1.val <= l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+};
+
+var mergeTwoLists3 = function (l1, l2) {
+  var mergedHead = { val: -1, next: null },
+    crt = mergedHead;
+  while (l1 && l2) {
+    if (l1.val > l2.val) {
+      crt.next = l2;
+      l2 = l2.next;
+    } else {
+      crt.next = l1;
+      l1 = l1.next;
+    }
+    crt = crt.next;
+  }
+  crt.next = l1 || l2;
+
+  return mergedHead.next;
 };
