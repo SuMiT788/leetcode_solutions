69fd17ad9202e4d0c33754a6d1608ad4e567e3ae
Sumit Kale <sumitkale788@gmail.com>
2024-03-09 23:44:33 +0530

    Palindrome linked list

--- palindromeLinkedList.js
+++ palindromeLinkedList.js
@@ -45,3 +45,40 @@ var isPalindrome = function (head) {
 
   return true;
 };
+
+var isPalindrome2 = (head) => {
+  // Initialize pointers for slow, fast, prev, and next nodes
+  let slow = head,
+    fast = head,
+    prev = null,
+    next = head;
+
+  // Move fast pointer to the end of the list
+  while (fast.next) {
+    fast = fast.next; // Move fast pointer
+    next = slow.next; // Store next node for reversing
+    slow.next = prev; // Reverse the slow pointer's next node
+    prev = slow; // Update prev to current slow node
+    slow = next; // Move slow pointer
+    fast = fast.next; // Move fast pointer again
+    if (fast === null) {
+      break;
+    }
+  }
+
+  // If the list has odd number of nodes, move slow pointer one step ahead
+  if (fast) {
+    slow = slow.next;
+  }
+
+  // Compare reversed first half with the second half
+  while (slow) {
+    if (slow.val !== prev.val) {
+      return false; // Return false if values don't match
+    }
+    slow = slow.next; // Move slow pointer forward
+    prev = prev.next; // Move prev pointer forward
+  }
+
+  return true; // Return true if palindrome
+};
