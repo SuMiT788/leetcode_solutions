6216c83c9cd899775689c64d0d703529df64d7a5
Sumit Kale <sumitkale788@gmail.com>
2024-03-11 22:55:05 +0530

    Single Number

---
+++ singleNumber.js
@@ -0,0 +1,27 @@
+/**
+ * https://leetcode.com/problems/single-number
+ *
+ * Given a non-empty array of integers nums,
+ * every element appears twice except for one.
+ * Find that single one.
+ *
+ * You must implement a solution with a linear runtime complexity and
+ * use only constant extra space.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+  if (!nums.length) return null;
+
+  let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    result ^= nums[i];
+  }
+
+  return result;
+};
+
+console.log(singleNumber([4, 1, 2, 1, 2]));
